bb467b53e09effbe9d261d3fca221b09b4cb6f89
Jordan Youssef <jordanyoussef28@gmail.com>
2024-06-22 00:07:35 -0500

    added drop down menu to form

--- components/forms/addVocabForm.js
+++ components/forms/addVocabForm.js
@@ -14,10 +14,18 @@ const addVocabForm = (obj = {}) => {
         <label for="definition">Definition</label>
         <textarea class="form-control" placeholder="Vocab Definition" id="definition" style="height: 100px">${obj.definition || ''}</textarea>
       </div>
-      <div class="form-group">
-        <label for="language">Language</label>
-        <input type="text" class="form-control" id="language" placeholder="Language" value="${obj.language || ''}" required>
+     
+ <div class="form-group" id="select-language">
+       <label for="language" class="category">Language</label>
+        <select class="form-control" placeholder="Select Category" id="language" name="vocabLanguage" value="${obj.language || ''}" required>
+        <option value="">Select Language</option>
+          <option value="HTML" ${obj.language === 'HTML' ? 'selected' : ''}>HTML</option>
+          <option value="JavaScript" ${obj.language === 'JavaScript' ? 'selected' : ''}>JavaScript</option>
+          <option value="Python" ${obj.language === 'Python' ? 'selected' : ''}>Python</option>
+        </select>
       </div>
+
+
       </div>
   
       <button type="submit" class="btn btn-primary" id="submit-btn">Submit Vocab
